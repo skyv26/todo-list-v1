7e501bfe59b4133681461980d7eb1009d6e33631
skyv26 <dev.aakashv@gmail.com>
2022-08-12 21:27:46 +0530

    Add `sorting` to display content according to index

--- src/index.js
+++ src/index.js
@@ -7,19 +7,21 @@ const todosTaskList = [
   {
     description: 'Morning walk at 5AM with brother',
     completed: false,
-    index: 1,
+    index: 2,
   },
   {
     description: 'Go to GYM for weight loss',
     completed: false,
-    index: 2,
+    index: 3,
   },
   {
     description: 'Buy some groceries from city',
     completed: false,
-    index: 3,
+    index: 1,
   },
 ];
+
+todosTaskList.sort((a, b) => a.index - b.index);
 
 todosTaskList.forEach((each) => {
   const htmlTemplateLiteral = `
