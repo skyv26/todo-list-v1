250b3fb895fb550072adf852bcebe815796a6f0c
skyv26 <dev.aakashv@gmail.com>
2022-08-12 15:07:35 +0530

    Add `webpack config` file both for development and production environment

---
+++ webpack.dev.js
@@ -0,0 +1,14 @@
+
+const common = require('./webpack.common');
+const { merge } = require('webpack-merge');
+const path = require('path');
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: false,
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'assets/[contenthash][ext][query]',
+  }
+});
