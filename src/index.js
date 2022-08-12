1d526c6e733962521e0225ba3175cc9517f8032f
skyv26 <dev.aakashv@gmail.com>
2022-08-12 20:06:02 +0530

    Add array of objects for todo and create function

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import './index.scss';
+
+const listContainer = document.querySelector('.todo__main-taskContainer');
+const refreshButton = document.querySelector('.todo__header-refreshButton');
+
+const todosTaskList = [
+  {
+    description: 'Morning walk at 5AM with brother',
+    completed:  false,
+    index: 1,
+  },
+  {
+    description: 'Go to GYM for weight loss',
+    completed:  false,
+    index: 2,
+  },
+  {
+    description: 'Buy some groceries from city',
+    completed:  false,
+    index: 3,
+  },
+];
+
+todosTaskList.forEach((each) => {
+  let htmlTemplateLiteral = `
+    <li class="space todo__list" id="list-${each.index}" draggable="true" onclick="todoListHandler(event, this)">
+      <input class="todo__list-status" type="checkbox" name="todo__task_checkbox" ${each.completed ? `checked` : ''} id="todo__task_checkbox-${each.index}" aria-label="mark the task as completed or uncompleted" >
+      <label for="task-${each.index}" class="todo__task-placeholder" onclick="todoEditHandler(this)">
+        <span class="strike">${each.description}</span>
+        <input class="update-todo__input" type="text" name="task-${each.index}" id="task-${each.index}" >
+      </label>
+      <div class="todo__mode">
+        <i class="fontawesome-icon fa-solid fa-ellipsis-vertical ellipse" id="todo__mode-${each.index}"></i>
+      </div>
+    </li>
+  `;
+  listContainer.insertAdjacentHTML('beforeend',htmlTemplateLiteral);
+});
+
+refreshButton.addEventListener('click', (e) => {
+  location.reload();
+})
