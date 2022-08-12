9334b7f839754b19fa0910a745729f826920964f
skyv26 <dev.aakashv@gmail.com>
2022-08-12 23:44:15 +0530

    Refactor `TodoLib` file

--- src/store/TodoLib.js
+++ src/store/TodoLib.js
@@ -1,32 +1,36 @@
 import LocalStore from './LocalStore';
 
-class TodoLib extends LocalStore {
-  constructor() {
-    super();
-    this.todoListArray = [];
-    this.listSize = 0;
+const TodoLib = () => {
+  class Todo extends LocalStore {
+    constructor() {
+      super();
+      this.todoListArray = [];
+      this.listSize = 0;
+    }
+
+    getUniqueId() {
+      this.listSize += 1;
+      return this.listSize;
+    }
+
+    createTodoObj(text) {
+      return { description: text, completed: false, index: this.getUniqueId() };
+    }
+
+    addTodo(todoString) {
+      this.todoListArray.unshift(this.createTodoObj(todoString));
+      this.setLocalStorageData(this.todoListArray);
+      const temp = this.map((each, indx) => {
+        each.index = indx + 1;
+        return each;
+      });
+      this.todoListArray = temp;
+      this.todoCollector.sort((a, b) => a.index - b.index);
+      this.listSize = this.todoCollector.length;
+    }
   }
 
-  getUniqueId() {
-    this.listSize += 1;
-    return this.listSize;
-  }
-
-  createTodoObj(text) {
-    return { description: text, completed: false, index: this.getUniqueId() };
-  }
-
-  addTodo(todoString) {
-    this.todoListArray.unshift(this.createTodoObj(todoString));
-    this.setLocalStorageData(this.todoListArray);
-    const temp = this.map((each, indx) => {
-      each.index = indx + 1;
-      return each;
-    });
-    this.todoListArray = temp;
-    this.todoCollector.sort((a, b) => a.index - b.index);
-    this.listSize = this.todoCollector.length;
-  }
+  return new Todo();
 }
 
 export default TodoLib;
