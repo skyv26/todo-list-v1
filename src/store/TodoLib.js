5d090387494f25642391176bbdbadf004fd98035
skyv26 <dev.aakashv@gmail.com>
2022-08-12 23:30:24 +0530

    Add `todo-add` functionality

---
+++ src/store/TodoLib.js
@@ -0,0 +1,32 @@
+import LocalStore from './LocalStore';
+
+class TodoLib extends LocalStore {
+  constructor() {
+    super();
+    this.todoListArray = [];
+    this.listSize = 0;
+  }
+
+  getUniqueId() {
+    this.listSize += 1;
+    return this.listSize;
+  }
+
+  createTodoObj(text) {
+    return { description: text, completed: false, index: this.getUniqueId() };
+  }
+
+  addTodo(todoString) {
+    this.todoListArray.unshift(this.createTodoObj(todoString));
+    this.setLocalStorageData(this.todoListArray);
+    const temp = this.map((each, indx) => {
+      each.index = indx + 1;
+      return each;
+    });
+    this.todoListArray = temp;
+    this.todoCollector.sort((a, b) => a.index - b.index);
+    this.listSize = this.todoCollector.length;
+  }
+}
+
+export default TodoLib;
